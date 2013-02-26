73ce6345d16cd41f897cb40fc60d772ecb6e5133
zolkko <zolkko@gmail.com>
2013-02-27 03:05:29 +0400

    minor changes

--- presentation/content/site.js
+++ presentation/content/site.js
@@ -1,3 +1,14 @@
+/*
+ * vim:set et tabstop=4 shiftwidth=4 nowrap fileencoding=utf-8:
+ */
 
 ;(function ($) {
+    $(function () {
+        $(".tabs").click(function (e) {
+			$(this).find("li.ui-state-active").removeClass("ui-state-active");
+			$(e.target).parents("li").addClass("ui-state-active");
+			e.preventDefault();
+		});
+	});
 }) (jQuery);
+
--- roboserv/roboserv/signallingchannel.py
+++ roboserv/roboserv/signallingchannel.py
@@ -2,6 +2,7 @@
 # vim:set et tabstop=4 shiftwidth=4 nu nowrap fileencoding=utf-8:
 
 import logging as log
+import json
 from tornado.websocket import WebSocketHandler
 
 
@@ -10,20 +11,55 @@ __all__ = ['SignallingChannelHandler']
 
 STATE_UNKNOWN = 0x00
 STATE_START = 0x01
-STATE_SDP_HANDSHAKE = 0x02
+STATE_SDP_DESCR = 0x02
+STATE_SDP_CANDIDATE = 0x03
+STATE_DONE = 0x04
+STATE_ERROR  = 0xfe
 STATE_CLOSED = 0xff
 
 
+class Candidate(object):
+    sdp_mline = 0
+    sdp_mid = u''
+    candidate = None
+
+
 class SignallingChannelHandler(WebSocketHandler):
-    state = STATE_UNKNOWN
+    state = STATE_START
     
     def open(self):
-        self.state = STATE_START
+        self.state = STATE_SDP_DESCR
         log.info('Signalling channle was opened.')
     
     def on_message(self, message):
-        log.info('message: {0}.'.format(message))
-        self.write_message(message)
+        log.info('Message has been received on signalling channel:\r\n{0}.'.format(message))
+        if self.state == STATE_SDP_DESCR :
+            self.on_session_description(message)
+        elif self.state == STATE_SDP_CANDIDATE :
+            self.on_candidate(message)
+        else :
+            log.warn('Unsupported state {0}. Signalling message will be ignored.'.format(self.state))
+    
+    def on_session_description(self, msg):
+        """
+        Method determines which media streams are available and
+        sends session description back to web-application.
+
+        @type msg: C{str}
+        @param msg: Session description
+        """
+        self.state = STATE_SDP_CANDIDATE
+        self.write_message(msg)
+    
+    def on_candidate(self, msg) :
+        """
+        This method selects most approprivate candidate.
+
+        @type msg: C{str}
+        @param msg: SDP candidate
+        """
+        self.state = STATE_SDP_CANDIDATE
+        self.write_message(msg)
     
     def on_close(self):
         log.info('Signalling channel has been closed.')
